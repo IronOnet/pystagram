12a5ed918400274187312a5ea975d2cd9cc1c9c7
ironOnet <arnaudwanetwork9@gmail.com>
2022-05-26 02:32:33 +0100

    added new components

---
+++ frontend/src/components/sidebar/user.js
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types'; 
+import {Link } from 'react-router-dom'; 
+import Skeleton from 'react-loading-skeleton'; 
+import { DEFAULT_IMAGE_PATH } from '../../constants/paths'; 
+
+export default function User({username, fullName}){
+    //TODO: WIP
+}
